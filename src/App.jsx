48d3d4c7c737d117366577110b97ac8dadf62e41
Aakash265 <aksverma26@gmail.com>
2023-08-17 14:08:55 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { motion, useScroll } from "framer-motion";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 
 const App = () => {
-    const { scrollYProgress } = useScroll();
   return (
     <div className="relative z-0 bg-neutral-700">
       <div className="content h-screen">
-      <motion.div
-        className="progress-bar"
-        style={{ scaleX: scrollYProgress }}
-      />
         <Navbar />
         <Hero />
         <About />
